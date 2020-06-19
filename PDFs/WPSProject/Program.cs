23d741a360aaf358683c2dd9b4aa5a06e1d47c20
baominxing <baominxing@foxmail.com>
2020-06-19 17:47:10 +0800

    fix : 添加WPS项目

---
+++ PDFs/WPSProject/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPSProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Extension = ".xls";//扩展名 ".aspx"
+            switch (Extension)
+            {
+                case ".xls":
+                    Extension = "KET.Application";
+                    break;
+                case ".xlsx":
+                    Extension = "KET.Application";
+                    break;
+                case ".ppt":
+                    Extension = "KWPP.Application";
+                    break;
+                case ".pptx":
+                    Extension = "KWPP.Application";
+                    break;
+                default:
+                    Extension = "KWps.Application";
+                    break;
+            }
+            Type type = Type.GetTypeFromProgID(Extension);
+
+            if (type == null)
+            {
+                Console.WriteLine("Type.GetTypeFromProgID(Extension) == NULL");
+
+                type = Type.GetTypeFromProgID("wps.Application");
+
+                if (type == null)
+                {
+                    Console.WriteLine("Type.GetTypeFromProgID(wps.Application) == NULL");
+                }
+            }
+            else
+            {
+                Console.WriteLine(type.Name);
+            }
+            
+            var wps = Activator.CreateInstance(type);
+
+            if (wps == null)
+            {
+                Console.WriteLine("wps == NULL");
+            }
+            else
+            {
+                Console.WriteLine(wps.GetType().Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
