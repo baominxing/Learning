9b772071378e63379f98e072230b48edebe2f201
baominxing <baominxing@foxmail.com>
2020-11-07 10:52:22 +0800

    fix : modify webservice project

--- WebServices/WebService20200325/WebService20200325001.asmx.cs
+++ WebServices/WebService20200325/WebService20200325001.asmx.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Web.Services.Description;
+using System.Web.Services.Protocols;
+using System.Xml.Serialization;
 
 namespace WebService20200325
 {
@@ -16,13 +19,43 @@ namespace WebService20200325
     // [System.Web.Script.Services.ScriptService]
     public class WebService20200325001 : System.Web.Services.WebService
     {
+        //[XmlNamespaceDeclarations]
+        //public XmlSerializerNamespaces xmlns
+        //{
+        //    get
+        //    {
+        //        XmlSerializerNamespaces xsn = new XmlSerializerNamespaces();
+        //        xsn.Add("me", "http://mynamespace/");
+        //        return xsn;
+        //    }
+        //    set { /* needed for xml serialization */ }
+        //}
 
-        [WebMethod]
-        public string HelloWorld()
+        //public WebService20200325001()
+        //{
+        //    xmlns = new XmlSerializerNamespaces();
+        //    xmlns.Add("MyNS", "myNS.tempuri.org");
+        //}
+        [SoapDocumentMethod("http://tempuri.org/GlobalSearchService", RequestNamespace = "http://tempuri.org.com/", ResponseNamespace = "http://tempuri.org/", Use = SoapBindingUse.Literal, ParameterStyle = SoapParameterStyle.Wrapped)]
+        [WebMethod(Description = "Obtains the User Name")]
+        public string HelloWorld(HelloWordDto helloWordDto)
         {
-            System.Diagnostics.Process.Start("iexplore.exe", "http://blog.csdn.net/testcs_dn");
-
-            return "Hello World";
+            return helloWordDto?.Name ?? string.Empty;
         }
+    }
+
+    [Serializable]
+    public class HelloWordDto
+    {
+        [XmlNamespaceDeclarations]
+        public XmlSerializerNamespaces xmlns;
+        public HelloWordDto()
+        {
+            xmlns = new XmlSerializerNamespaces();
+            xmlns.Add("MyNS", "myNS.tempuri.org");
+            xmlns.Add("common", "common.tempuri.org");
+        }
+
+        public string Name { get; set; }
     }
 }
