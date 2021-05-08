b95f37690c9a897f0b1065aa34db256d144c13a0
baominxing <baominxing@foxmail.com>
2021-05-09 07:20:59 +0800

    feat : add windbg thread_static project

---
+++ CSharps/Windbg_ThreadStatic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windbg_ThreadStatic
+{
+    class Program
+    {
+        [ThreadStatic]
+        public static int i = 0;
+
+        static void Main(string[] args)
+        {
+            i = 10;   // 12 line
+
+            var num = i;
+
+            Console.ReadLine();
+        }
+    }
+}
